3d51d8fa2fd8a260929b3400b17bf4b81eef86a0
SeniorCub <farinderif@gmail.com>
2024-06-05 00:21:11 +0100

    "Refactored server.js: rearranged imports, moved express app initialization, updated static file serving, added 404 error middleware, and modified console log message."

--- server.js
+++ server.js
@@ -1,32 +1,30 @@
 //Server
 import path from 'path';
 import express from 'express';
-const app = express();
-
-//Connecting to database (MongoDB)
 import dotenv from 'dotenv';
-
 import connectdB from './dB/connect.js';
-
 import morgan from 'morgan';
-
 import blogroute from './routes/blogroute.js';
 
+const app = express();
 dotenv.config();
 connectdB();
 
-//For getting user info from the sign up page
+
 app.use(express.json());
 app.use(morgan("dev"))
 app.use(express.urlencoded({ extended: false }));
+app.use("/", express.static(path.join(process.cwd(), "/Public")))
+app.use("/uploads", express.static(path.join(process.cwd(), "/uploads")))
 
-//To load my server
-app.use('/', express.static(path.join(process.cwd(), '/public')));
-app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
+// Middleware for handling 404 errors
+app.use((req, res, next) => {
+     res.status(404).sendFile(path.join(process.cwd(), "/View/404.html"));
+});
 
 app.use('/api/v1/blog', blogroute);
 
 const PORT = process.env.PORT || 7000
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`.bgMagenta);
+     console.log(`Server running on http://localhost:${PORT}`.bgMagenta);
 });
