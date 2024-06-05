a237dd53f7561934c7d051c8b92edd0d91ff739e
SeniorCub <farinderif@gmail.com>
2024-06-05 18:04:59 +0100

    "Updated login.html with new Bootstrap integrity hash and changed API endpoint URL; updated server.js to reorder middleware and route usage"

--- server.js
+++ server.js
@@ -21,13 +21,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/", express.static(path.join(process.cwd(), "/Public")))
 app.use("/uploads", express.static(path.join(process.cwd(), "/uploads")))
 
-// Middleware for handling 404 errors
-app.use((req, res, next) => {
-     res.status(404).sendFile(path.join(process.cwd(), "/View/404.html"));
-});
-
 app.use('/api/v1/auth', authroute);
 app.use('/api/v1/blog', blogroute);
+
+// Middleware for handling 404 errors
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(process.cwd(), "/View/404.html"));
+});
 
 const PORT = process.env.PORT || 7000
 app.listen(PORT, () => {
