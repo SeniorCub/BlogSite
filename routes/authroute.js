760549eab532bc55077f35160279d95dd4926653
SeniorCub <farinderif@gmail.com>
2024-06-08 01:08:58 +0100

    "Updated auth controller, added getUsers function, modified login token expiration, refactored router, and updated adminss.html and login.html files"

--- routes/authroute.js
+++ routes/authroute.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { registerClient, loginClient, getUsers } from '../controller/authcontroller.js';
 import { isAdmin, signinController } from "../middlewares/signinController.js";
+import { requireAuth } from "../middlewares/logoutController.js"
 const router = express.Router();
 
 router.use(express.json());
@@ -10,7 +11,12 @@ router.post('/signUp', registerClient);
 
 router.post('/login', loginClient);
 
-router.get('/users', getUsers)
+router.get('/users', getUsers);
+
+router.get('/protected-route', requireAuth, (req, res) => {
+    res.send('This is a protected route');
+});
+
 
 router.get('/user', signinController, isAdmin)
 
