4481a270cc0026f067ad5fc3d24e79aba4fc20e8
seniorcub <farinderif@gmail.com>
2024-11-01 11:17:29 -0400

    New commit on ubuntu

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ dotenv.config();
 connectdB();
 
 app.use(express.json());
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: false }));
-app.use("/", express.static(path.join(process.cwd(), "/public")))
-app.use("/uploads", express.static(path.join(process.cwd(), "/uploads")))
+app.use("/", express.static(path.join(process.cwd(), "/public")));
+app.use("/uploads", express.static(path.join(process.cwd(), "/uploads")));
 
 app.use('/api/v1/auth', authroute);
 app.use('/api/v1/blog', blogroute);
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
     res.status(404).sendFile(path.join(process.cwd(), "/View/404.html"));
 });
 
-const PORT = process.env.PORT || 7000
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
      console.log(`Server running on http://localhost:${PORT}`.bgMagenta);
 });
